5ffe24ef89c1bf1b96efeda29f9d3c04b37e13fc
Gianni Grifone <gianni.grifone@gmx.ch>
2025-04-22 18:12:06 +0200

    Pendel 2.0

--- Pendel.py
+++ Pendel.py
@@ -12,17 +12,23 @@ balls=[]
 g=981
 l=200
 font=pygame.font.Font(None,30)
+font1=pygame.font.Font(None,20)
 icon=pygame.image.load("IconSkaliert.png")
 pygame.display.set_icon(icon)
 ic=0
+thetag=45
+theta0=np.radians(thetag)
 while not exit:
     canvas.fill("white")
     if len(balls)>0:
         text_surface=font.render("Länge="+str(l)+"cm",True,"black")
-        text_rect=text_surface.get_rect(center=(70,40))
+        text_rect=text_surface.get_rect(center=(90,40))
         canvas.blit(text_surface,text_rect)
         text_surface=font.render("Anzahl Pendel: "+str(len(balls)),True,"black")
-        text_rect=text_surface.get_rect(center=(85,60))
+        text_rect=text_surface.get_rect(center=(105,60))
+        canvas.blit(text_surface,text_rect)
+        text_surface=font.render("Auslenkungswinkel: "+str(thetag)+"°",True,"black")
+        text_rect=text_surface.get_rect(center=(140,80))
         canvas.blit(text_surface,text_rect)
     tick=pygame.time.get_ticks()
     for event in pygame.event.get():
@@ -35,6 +41,14 @@ while not exit:
             if event.button==3:
                 cor2=pygame.mouse.get_pos()
                 ic+=1
+            if event.button==4:
+                if thetag<180:
+                    thetag+=5
+                    theta0=np.radians(thetag)
+            if event.button==5:
+                if thetag>0:
+                    thetag-=5
+                    theta0=np.radians(thetag)
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_BACKSPACE:
                 if len(balls)>0:
@@ -50,18 +64,31 @@ while not exit:
             WIDTH=event.w
             HEIGHT=event.h   
     if ic%2 !=0 :
-        text_surface=font.render("Delete drücken um Pendel zu löschen",True,("black"))
+        text_surface=font1.render("Delete drücken um Pendel zu löschen",True,("black"))
         text_rect=text_surface.get_rect(center=(cor2))
         canvas.blit(text_surface,text_rect)
-        text_surface=font.render("Zurück drücken um letztes Pendel zu löschen",True,("black"))
+        text_surface=font1.render("Zurück drücken um letztes Pendel zu löschen",True,("black"))
         text_rect=text_surface.get_rect(center=(cor2[0],cor2[1]+30))
         canvas.blit(text_surface,text_rect)
+        text_surface=font1.render("Hoch scrollen um Auslenkungswinkel zu erhöhen",True,("black"))
+        text_rect=text_surface.get_rect(center=(cor2[0],cor2[1]+60))
+        canvas.blit(text_surface,text_rect)
+        text_surface=font1.render("Runter scrollen um Auslenkungswinkel zu verkleinern",True,("black"))
+        text_rect=text_surface.get_rect(center=(cor2[0],cor2[1]+90))
+        canvas.blit(text_surface,text_rect)
+        text_surface=font1.render("Pfeiltaste Rechts um Länge zu erhöhen",True,("black"))
+        text_rect=text_surface.get_rect(center=(cor2[0],cor2[1]+120))
+        canvas.blit(text_surface,text_rect)
+        text_surface=font1.render("Pfeiltaste Links um Länge zu verkleinern",True,("black"))
+        text_rect=text_surface.get_rect(center=(cor2[0],cor2[1]+150))
+        canvas.blit(text_surface,text_rect)
     for ball in balls:
-        u=np.sqrt(g/l)*(tick-ball["t0"])/900
-        k=0.4
+        u=np.sqrt(g/l)*(tick-ball["t0"])/1000
+        k=np.sin(theta0/2)
         sn, cn, dn, am = sp.ellipj(u, k)
-        x=l*np.sin(sn)
-        y=l*np.cos(sn)
+        theta=2*np.arcsin(k*sn)
+        x=l*np.sin(theta)
+        y=l*np.cos(theta)
 
         ort=(ball["x0"]+x,ball["y0"]+y)
         pygame.draw.line(canvas,"black",(ball["x0"],ball["y0"]), ort,10)
